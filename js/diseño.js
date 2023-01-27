6c7e1e9b02f3a0b52b02a2a700a624ca7d8a92c6
Jank1510 <jeanhurt15@gmail.com>
2023-01-26 20:09:01 -0500

    update structure js

---
+++ js/diseño.js
@@ -0,0 +1,87 @@
+let tema = 2 
+function temas() {
+    //elementos por id
+    let btn1 = document.getElementById("tema1")
+    let btn2 = document.getElementById("tema2")
+    let btn3 = document.getElementById("tema3")
+    let entrada = document.getElementById("Entrada")
+    let contenedorBTN = document.getElementById("contenedorBTN")
+    let padre = document.getElementById("padre")
+    let operaciones = document.getElementById("Operaciones")
+    let cabezera = document.getElementById("Cabezera")
+    //elementos po clases
+    let BtnCalculadora = document.getElementsByClassName("btnCal")
+    let shadowDelReset = document.getElementsByClassName("shadowDelReset")
+    let resultado = document.getElementById("resultado")
+    let pantalla = document.getElementById("numerosP")
+
+    if (tema === 1) {
+        pantalla.style.color = "white"
+        padre.style.backgroundColor = "hsl(222, 26%, 31%)"
+        operaciones.style.backgroundColor = "hsl(223, 31%, 20%)"
+        contenedorBTN.style.backgroundColor = "hsl(223, 31%, 20%)"
+        btn1.style.backgroundColor = "hsl(6, 63%, 50%)"
+        btn2.style.backgroundColor = "hsl(223, 31%, 20%)"
+        btn3.style.backgroundColor = "hsl(223, 31%, 20%)"
+        entrada.style.backgroundColor = "hsl(224, 36%, 15%)"
+        cabezera.style.color = "white"
+        let cssbtnCal = "box-shadow: 0px 6px 0px 0px hsl(28, 16%, 65%);background-color:hsl(30, 25%, 89%);color:hsl(221, 14%, 31%)"
+        let cssShadowDelRes = "box-shadow: 0px 6px 0px 0px  hsl(224, 28%, 35%);background-color:hsl(225, 21%, 49%)"
+        for (let i = 0; i < BtnCalculadora.length; i++) {
+            BtnCalculadora[i].style.cssText = cssbtnCal;
+        }
+        for (let i = 0; i < shadowDelReset.length; i++) {
+            shadowDelReset[i].style.cssText = cssShadowDelRes;
+        }
+        let cssresultado = "box-shadow: 0px 6px 0px 0px hsl(6, 70%, 34%);background-color:hsl(6, 63%, 50%)"
+        resultado.style.cssText = cssresultado
+        tema=2
+    } else {
+        if (tema === 2) {
+            pantalla.style.color = "hsl(60, 10%, 19%)"
+            padre.style.backgroundColor = "hsl(0, 0%, 90%)"
+            operaciones.style.backgroundColor = "hsl(0, 5%, 81%)"
+            contenedorBTN.style.backgroundColor = "hsl(0, 5%, 81%)"
+            btn1.style.backgroundColor = "hsl(0, 5%, 81%)"
+            btn2.style.backgroundColor = "hsl(25, 98%, 40%)"
+            btn3.style.backgroundColor = "hsl(0, 5%, 81%)"
+            entrada.style.backgroundColor = "hsl(0, 0%, 93%)"
+            cabezera.style.color = "hsl(60, 10%, 19%)"
+            let cssbtnCal = "box-shadow: 0px 6px 0px 0px hsl(35, 11%, 61%);background-color:hsl(45, 7%, 89%);color:hsl(60, 10%, 19%)"
+            let cssShadowDelRes = "box-shadow: 0px 6px 0px 0px  hsl(185, 58%, 25%);background-color:hsl(185, 42%, 37%)"
+            for (let i = 0; i < BtnCalculadora.length; i++) {
+                BtnCalculadora[i].style.cssText = cssbtnCal;
+            }
+            for (let i = 0; i < shadowDelReset.length; i++) {
+                shadowDelReset[i].style.cssText = cssShadowDelRes;
+            }
+            let cssresultado = "box-shadow: 0px 6px 0px 0px hsl(25, 99%, 27%);background-color:hsl(25, 98%, 40%)"
+            resultado.style.cssText = cssresultado
+            tema=3
+        } else {
+            if (tema === 3) {
+                pantalla.style.color = "hsl(52, 100%, 62%)"
+                padre.style.backgroundColor = " hsl(268, 75%, 9%)"
+                operaciones.style.backgroundColor = "hsl(268, 71%, 12%)"
+                contenedorBTN.style.backgroundColor = "hsl(268, 71%, 12%)"
+                btn1.style.backgroundColor = "hsl(268, 71%, 12%)"
+                btn2.style.backgroundColor = "hsl(268, 71%, 12%)"
+                btn3.style.backgroundColor = "hsl(176, 100%, 44%)"
+                entrada.style.backgroundColor = "hsl(268, 71%, 12%)"
+                cabezera.style.color = "hsl(52, 100%, 62%)"
+                let cssbtnCal = "box-shadow: 0px 6px 0px 0px hsl(290, 70%, 36%);background-color:hsl(268, 47%, 21%);color:hsl(52, 100%, 62%)"
+                let cssShadowDelRes = "box-shadow: 0px 6px 0px 0px hsl(285, 91%, 52%);background-color:hsl(281, 89%, 26%)"
+                for (let i = 0; i < BtnCalculadora.length; i++) {
+                    BtnCalculadora[i].style.cssText = cssbtnCal;
+                }
+                for (let i = 0; i < shadowDelReset.length; i++) {
+                    shadowDelReset[i].style.backgroundColor = "";
+                    shadowDelReset[i].style.cssText = cssShadowDelRes;
+                }
+                let cssresultado = "box-shadow: 0px 6px 0px 0px hsl(177, 92%, 70%);background-color: hsl(176, 100%, 44%);color:hsl(198, 20%, 13%)"
+                resultado.style.cssText = cssresultado
+                tema=1
+            }
+        }
+    }
+}
---
+++ js/logica.js
@@ -0,0 +1,106 @@
+let numeros = [] 
+let condiciondoperadorsincontrol//variable para que no se opere cuando se preciona varias veces un operador si no que se realice una sola vez hasta oprimir otro numero
+let valores = (numero) => {//funcion que imprime los valores en pantalla
+    condiciondoperadorsincontrol = true
+    let pantalla = document.getElementById("numerosP")
+    if (numeros.length < 14) {
+        numeros.push(numero)
+    }
+    let numeroArray = numeros.toString().replace(/,/g, "")
+    pantalla.innerHTML = numeroArray
+}
+let numerosIngresados = []//arreglo que almacena los datos que se operan
+let resultadelosdosnumeros
+let operadorPendiente = ''//string que almacena que operador es seleccionado
+let operacion = (tipoOperacion) => {//funcion que condiciona el operador seleccionado
+    let numerodigitado = parseFloat(document.getElementById("numerosP").innerText)
+    numerosIngresados.push(numerodigitado)
+    numeros = []
+    switch (tipoOperacion) {
+        case '+':
+            operacionPendiente()
+            operadorPendiente = '+'
+            break
+        case '-':
+            operacionPendiente()
+            operadorPendiente = '-'
+            break
+        case '*':
+            operacionPendiente()
+            operadorPendiente = '*'
+            break
+        case '/':
+            operacionPendiente()
+            operadorPendiente = '/'
+            break
+    }
+    document.getElementById("numerosP").innerHTML = (numerosIngresados)
+}
+let operacionPendiente = () => {//funcion que realiza los calculos dependiendo de la var operadorpendiente
+    if (operadorPendiente == '+') {
+        numerosIngresados = [numerosIngresados[0] + numerosIngresados[1]]
+
+    } else {
+        if (operadorPendiente == '-') {
+            numerosIngresados = [numerosIngresados[0] - numerosIngresados[1]]
+
+        } else {
+            if (operadorPendiente == '*') {
+                numerosIngresados = [numerosIngresados[0] * numerosIngresados[1]]
+
+            } else {
+                if (operadorPendiente == '/') {
+                    numerosIngresados = [numerosIngresados[0] / numerosIngresados[1]]
+
+                }
+            }
+        }
+    }
+}
+//funciones que llevan la logica de los procesos de datos 
+function suma() {
+    if ((operadorPendiente != '+' || condiciondoperadorsincontrol == true) && document.getElementById("numerosP").innerText != '') {
+        operacion('+')
+        condiciondoperadorsincontrol = false
+    }
+}
+function resta() {
+    if ((operadorPendiente != '-' || condiciondoperadorsincontrol == true) && document.getElementById("numerosP").innerText != '') {
+        operacion('-')
+        condiciondoperadorsincontrol = false
+    }
+}
+function multiplicacion() {
+    if ((operadorPendiente != '*' || condiciondoperadorsincontrol == true) && document.getElementById("numerosP").innerText != '') {
+        operacion('*')
+        condiciondoperadorsincontrol = false
+    }
+}
+function dividir() {
+    if ((operadorPendiente != '/' || condiciondoperadorsincontrol == true) && document.getElementById("numerosP").innerText != '') {
+        operacion('/')
+        condiciondoperadorsincontrol = false
+    }
+}
+function del() {//funcion para eliminar el ultimo digito
+    let numerosEnPantalla = document.getElementById("numerosP").innerText
+    document.getElementById("numerosP").innerHTML = numerosEnPantalla.slice(0, -1);
+    numeros.pop()
+}
+let resultado = () => {
+    if (document.getElementById("numerosP").innerText != '') {
+        operacion(operadorPendiente)
+        document.getElementById("numerosP").innerHTML = (numerosIngresados)
+        numerosIngresados.pop()
+        operadorPendiente = ''
+    }
+}
+function reset() {
+    operadorPendiente = ''
+    numerosIngresados = []
+    resultadelosdosnumeros
+    operadorPendiente = ''
+    document.getElementById("numerosP").innerHTML = ''
+    numeros = []
+
+}
