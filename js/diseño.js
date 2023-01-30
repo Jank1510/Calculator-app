385adb9697e4bbb5c2ee5d3e31aa4c1b0b276682
Jank1510 <jeanhurt15@gmail.com>
2023-01-30 17:22:48 -0500

    update Redesign

--- js/diseño.js
+++ js/diseño.js
@@ -14,72 +14,107 @@ function temas() {
     let shadowDelReset = document.getElementsByClassName("shadowDelReset")
     let resultado = document.getElementById("resultado")
     let pantalla = document.getElementById("numerosP")
-
     if (tema === 1) {
         pantalla.style.color = "white"
+        pantalla.style.animationName = 'colortxt_3-1'
         padre.style.backgroundColor = "hsl(222, 26%, 31%)"
+        padre.style.animationName = 'animationColorsPadre_3-1'
         operaciones.style.backgroundColor = "hsl(223, 31%, 20%)"
+        operaciones.style.animationName = 'animationColorsOperaciones_3-1'
         contenedorBTN.style.backgroundColor = "hsl(223, 31%, 20%)"
         btn1.style.backgroundColor = "hsl(6, 63%, 50%)"
+        btn1.style.display = 'none'
+        setTimeout(() => {
+            btn1.style.display = 'block'
+        }, 5);
+        btn1.style.animationName = 'btnMovimiento_3-1'
         btn2.style.backgroundColor = "hsl(223, 31%, 20%)"
         btn3.style.backgroundColor = "hsl(223, 31%, 20%)"
         entrada.style.backgroundColor = "hsl(224, 36%, 15%)"
+        entrada.style.animationName = 'animationColorsOperacionesandENtrada_3-1'
         cabezera.style.color = "white"
         let cssbtnCal = "box-shadow: 0px 0.4vw 0px 0px hsl(28, 16%, 65%);background-color:hsl(30, 25%, 89%);color:hsl(221, 14%, 31%)"
         let cssShadowDelRes = "box-shadow: 0px 0.4vw 0px 0px  hsl(224, 28%, 35%);background-color:hsl(225, 21%, 49%)"
         for (let i = 0; i < BtnCalculadora.length; i++) {
             BtnCalculadora[i].style.cssText = cssbtnCal;
+            BtnCalculadora[i].style.animationName = 'btnLigth_3-1'
         }
         for (let i = 0; i < shadowDelReset.length; i++) {
             shadowDelReset[i].style.cssText = cssShadowDelRes;
+            shadowDelReset[i].style.animationName = 'btntwo_3-1'
         }
         let cssresultado = "box-shadow: 0px 0.4vw 0px 0px hsl(6, 70%, 34%);background-color:hsl(6, 63%, 50%)"
         resultado.style.cssText = cssresultado
+        resultado.style.animationName = 'btnresultado_3-1'
         tema = 2
     } else {
         if (tema === 2) {
+            pantalla.style.animationName = 'colortxt_1-2'
             pantalla.style.color = "hsl(60, 10%, 19%)"
             padre.style.backgroundColor = "hsl(0, 0%, 90%)"
+            padre.style.animationName = 'animationColorsPadre_1-2'
             operaciones.style.backgroundColor = "hsl(0, 5%, 81%)"
+            operaciones.style.animationName = 'animationColorsOperaciones_1-2'
             contenedorBTN.style.backgroundColor = "hsl(0, 5%, 81%)"
             btn1.style.backgroundColor = "hsl(0, 5%, 81%)"
             btn2.style.backgroundColor = "hsl(25, 98%, 40%)"
+            btn2.style.display = 'none'
+            setTimeout(() => {
+                btn2.style.display = 'block'
+            }, 5);
+            btn2.style.animationName = 'btnMovimiento_1-2_and_2-3'
             btn3.style.backgroundColor = "hsl(0, 5%, 81%)"
             entrada.style.backgroundColor = "hsl(0, 0%, 93%)"
+            entrada.style.animationName = 'animationColorsEntrada_1-2'
             cabezera.style.color = "hsl(60, 10%, 19%)"
             let cssbtnCal = "box-shadow: 0px 0.4vw 0px 0px hsl(35, 11%, 61%);background-color:hsl(45, 7%, 89%);color:hsl(60, 10%, 19%)"
             let cssShadowDelRes = "box-shadow: 0px 0.4vw 0px 0px  hsl(185, 58%, 25%);background-color:hsl(185, 42%, 37%)"
             for (let i = 0; i < BtnCalculadora.length; i++) {
                 BtnCalculadora[i].style.cssText = cssbtnCal;
+                BtnCalculadora[i].style.animationName = 'btnLigth_1-2'
             }
             for (let i = 0; i < shadowDelReset.length; i++) {
                 shadowDelReset[i].style.cssText = cssShadowDelRes;
+                shadowDelReset[i].style.animationName = 'btntwo_1-2'
             }
             let cssresultado = "box-shadow: 0px 0.4vw 0px 0px hsl(25, 99%, 27%);background-color:hsl(25, 98%, 40%)"
             resultado.style.cssText = cssresultado
+            resultado.style.animationName = 'btnresultado_1-2'
             tema = 3
         } else {
             if (tema === 3) {
                 pantalla.style.color = "hsl(52, 100%, 62%)"
+                pantalla.style.animationName = 'colortxt_2-3'
                 padre.style.backgroundColor = " hsl(268, 75%, 9%)"
+                padre.style.animationName = 'animationColorsPadre_2-3'
                 operaciones.style.backgroundColor = "hsl(268, 71%, 12%)"
+                operaciones.style.animationName = 'animationColorsOperaciones_2-3'
                 contenedorBTN.style.backgroundColor = "hsl(268, 71%, 12%)"
                 btn1.style.backgroundColor = "hsl(268, 71%, 12%)"
                 btn2.style.backgroundColor = "hsl(268, 71%, 12%)"
                 btn3.style.backgroundColor = "hsl(176, 100%, 44%)"
+                btn3.style.display = 'none'
+                setTimeout(() => {
+                    btn3.style.display = 'block'
+                }, 5);
+                btn3.style.animationName = 'btnMovimiento_1-2_and_2-3'
                 entrada.style.backgroundColor = "hsl(268, 71%, 12%)"
+                entrada.style.animationName = 'animationColorsEntrada_2-3'
                 cabezera.style.color = "hsl(52, 100%, 62%)"
                 let cssbtnCal = "box-shadow: 0px 0.4vw 0px 0px hsl(290, 70%, 36%);background-color:hsl(268, 47%, 21%);color:hsl(52, 100%, 62%)"
                 let cssShadowDelRes = "box-shadow: 0px 0.4vw 0px 0px hsl(285, 91%, 52%);background-color:hsl(281, 89%, 26%)"
                 for (let i = 0; i < BtnCalculadora.length; i++) {
                     BtnCalculadora[i].style.cssText = cssbtnCal;
+                    BtnCalculadora[i].style.animationName = 'btnLigth_2-3'
                 }
                 for (let i = 0; i < shadowDelReset.length; i++) {
                     shadowDelReset[i].style.backgroundColor = "";
                     shadowDelReset[i].style.cssText = cssShadowDelRes;
+                    shadowDelReset[i].style.animationName = 'btntwo_2-3'
                 }
                 let cssresultado = "box-shadow: 0px 0.4vw 0px 0px hsl(177, 92%, 70%);background-color: hsl(176, 100%, 44%);color:hsl(198, 20%, 13%)"
                 resultado.style.cssText = cssresultado
+                resultado.style.animationName = 'btnresultado_2-3'
                 tema = 1;
             }
         }
@@ -92,49 +127,89 @@ let down = (id) => {
 let up = (id) => {
     document.getElementById(id).className = 'nopush btnCal'
 }
-//funciones para redimencion de tamaño de los numeros en pantalla
-
-
-let redimencionPresionarNumero = () => {
-    let anchoPantalla = document.getElementById('padre').clientWidth
-    let anchoentrada = document.getElementById('pantalla').clientWidth
+//funciones para redimencion de tamaño de los numeros en pantalla 
+let isbignumber = false
+let redimencionnumeros = (numIngresado) => {
+    let anchoDispositivo = document.getElementById("padre").clientWidth
     let numeros = document.getElementById('numerosP')
+    if (anchoDispositivo >= 770) {//se condiciona para verificar si la pantalla es de desktop ya q los fontsize varian
+        switch (((numIngresado.split("")).length)) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                numeros.style.fontSize = '6.2vw'
+                break
+            case 10:
+                numeros.style.fontSize = '5.7vw'
 
-    let fontSizeOriginal = parseFloat(window.getComputedStyle(numeros).fontSize)
-    if ((numeros.clientWidth * 100) / (anchoPantalla) > (anchoentrada * 100) / (anchoPantalla) - 3) {
-        numeros.style.fontSize = ((fontSizeOriginal * 100) / (anchoPantalla) - 1) + 'vw'
+            case 11:
+                numeros.style.fontSize = '5.3vw'
+                break
+            case 12:
+                numeros.style.fontSize = '4.8vw'
+
+            case 13:
+                numeros.style.fontSize = '4.4vw'
+
+            case 14:
+                numeros.style.fontSize = '3.9vw'
+                break
+            case 15:
+                numeros.style.fontSize = '3.5vw'
+                break
+            default:
+                isbignumber = true
+                break
+        }
+    } else {
+        if (anchoDispositivo < 770) {//se condiciona para verificar si la pantalla es de mobiles
+            switch (((numIngresado.split("")).length)) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    numeros.style.fontSize = '20.5vw'
+                    break
+                case 7:
+                    numeros.style.fontSize = '18.5vw'
+                    break
+                case 8:
+                    numeros.style.fontSize = '15.5vw'
+                    break
+                case 9:
+                    numeros.style.fontSize = '13.5vw'
+                    break
+                case 10:
+                    numeros.style.fontSize = '12.7vw'
+
+                case 11:
+                    numeros.style.fontSize = '11.7vw'
+                    break
+                case 12:
+                    numeros.style.fontSize = '11.5vw'
+
+                case 13:
+                    numeros.style.fontSize = '9.9vw'
+
+                case 14:
+                    numeros.style.fontSize = '8.9vw'
+                    break
+                case 15:
+                    numeros.style.fontSize = '8.5vw'
+                    break
+                default:
+                    isbignumber = true
+                    break
+            }
+        }
     }
-}
-let redimencionPresionarOperacion = (numIngresado) => {
-    let anchoPantalla = document.getElementById('padre').clientWidth
-    let anchoentrada = document.getElementById('pantalla').clientWidth
-    let numeros = document.getElementById('numerosP')
-    console.log(((numIngresado.split("")).length) + ' ' + numerosIngresados)
-
-    switch (((numIngresado.split("")).length)) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-            numeros.style.fontSize = '6.2vw'
-            break
-        case 10:
-        case 11:
-            numeros.style.fontSize = '5.2vw'
-            break
-        case 12:
-        case 13:
-        case 14:
-            numeros.style.fontSize = '4.2vw'
-            break 
-        default:
-            numeros.style.fontSize = '3.2vw'
-            break 
-    } 
 }
 
--- js/logica.js
+++ js/logica.js
@@ -1,22 +1,20 @@
-let numeros = [] 
+let numeros = []
 let condiciondoperadorsincontrol//variable para que no se opere cuando se preciona varias veces un operador si no que se realice una sola vez hasta oprimir otro numero
-let valores = (numero) => {//funcion que imprime los valores en pantalla
-    redimencionPresionarNumero()
-    
+let valores = (numero) => {//funcion que imprime los valores en pantalla 
+    isbignumber = false
     condiciondoperadorsincontrol = true
     let pantalla = document.getElementById("numerosP")
-    if (numeros.length < 17) {
+    if (numeros.length < 15) {//limita el numeros en pantalla q se pueden poner
         numeros.push(numero)
     }
-    let numeroArray = numeros.toString().replace(/,/g, "")
+    let numeroArray = numeros.toString().replace(/,/g, "")//convierte de lista a string
+    redimencionnumeros(numeroArray.toString())//redimenciona el tamaño de los numeros
     pantalla.innerHTML = numeroArray
 }
 let numerosIngresados = []//arreglo que almacena los datos que se operan
 let resultadelosdosnumeros
 let operadorPendiente = ''//string que almacena que operador es seleccionado
-
 let operacion = (tipoOperacion) => {//funcion que condiciona el operador seleccionado
-
     let numerodigitado = parseFloat(document.getElementById("numerosP").innerText)
     numerosIngresados.push(numerodigitado)
     numeros = []
@@ -38,26 +36,26 @@ let operacion = (tipoOperacion) => {//funcion que condiciona el operador selecci
             operadorPendiente = '/'
             break
     }
-    redimencionPresionarOperacion(numerosIngresados.toString())
+    redimencionnumeros(numerosIngresados.toString())//redimenciona el tamaño de los numeros
+    if (isbignumber != true) {
+        document.getElementById("numerosP").innerHTML = (numerosIngresados)
 
-    document.getElementById("numerosP").innerHTML = (numerosIngresados)
+    } else {
+        document.getElementById("numerosP").innerHTML = (parseFloat(numerosIngresados).toExponential((1)))//cuando la exprecion pasa el limite de numeors q se puedne mostrar se convierte en numeros exponenciales
+    }
 }
 let operacionPendiente = () => {//funcion que realiza los calculos dependiendo de la var operadorpendiente
     if (operadorPendiente == '+') {
         numerosIngresados = [numerosIngresados[0] + numerosIngresados[1]]
-
     } else {
         if (operadorPendiente == '-') {
             numerosIngresados = [numerosIngresados[0] - numerosIngresados[1]]
-
         } else {
             if (operadorPendiente == '*') {
                 numerosIngresados = [numerosIngresados[0] * numerosIngresados[1]]
-
             } else {
                 if (operadorPendiente == '/') {
                     numerosIngresados = [numerosIngresados[0] / numerosIngresados[1]]
-
                 }
             }
         }
@@ -90,13 +88,13 @@ function dividir() {
 }
 function del() {//funcion para eliminar el ultimo digito
     let numerosEnPantalla = document.getElementById("numerosP").innerText
-    document.getElementById("numerosP").innerHTML = numerosEnPantalla.slice(0, -1);
-    numeros.pop()
+    redimencionnumeros(numerosEnPantalla.slice(0, -1))//redimenciona el tamaño de los numeros
+    document.getElementById("numerosP").innerHTML = numerosEnPantalla.slice(0, -1);//elimina el ultimo digito
+    numeros.pop()//elimina el  ultimo numero de la cadena
 }
 let resultado = () => {
     if (document.getElementById("numerosP").innerText != '') {
         operacion(operadorPendiente)
-        document.getElementById("numerosP").innerHTML = (numerosIngresados)
         numerosIngresados.pop()
         operadorPendiente = ''
     }
@@ -108,5 +106,4 @@ function reset() {
     operadorPendiente = ''
     document.getElementById("numerosP").innerHTML = ''
     numeros = []
-
 }
